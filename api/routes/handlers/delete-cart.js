037d7afcf86c6b3d913853793ba72f223e5c6ca8
Juan David Om <anvidmen@gmail.com>
2020-08-03 13:46:49 +0200

    Added Api

---
+++ api/routes/handlers/delete-cart.js
@@ -0,0 +1,14 @@
+const { deleteCart } = require('server-logic')
+const { handleError } = require('../../helpers')
+
+module.exports = (req, res) => {
+  try {
+    const { payload: { sub: userId } } = req
+
+    deleteCart(userId)
+      .then(() => res.status(200).send())
+      .catch(error => handleError(error, res))
+  } catch (error) {
+    handleError(error, res)
+  }
+}
